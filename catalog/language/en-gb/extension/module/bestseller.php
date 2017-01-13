925768823e1a22d015f0556bc67e4ce22c50b179
nasser <nasser.man@gmail.com>
2017-01-13 11:39:39 +0330

    init

---
+++ catalog/language/en-gb/extension/module/bestseller.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Owl Bestsellers';
+
+// Text
+$_['text_tax']      = 'Ex Tax:';
---
+++ catalog/language/en-gb/extension/module/featured.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Owl Featured';
+
+// Text
+$_['text_tax']      = 'Ex Tax:';
